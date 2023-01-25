4f8e3cbd6f1bc7d1a1f489165ce2d908af3681ef
Steven Liu <53197799+l1usteven@users.noreply.github.com>
2023-01-25 10:19:56 -0500

    Merging typeWriter.js

--- scripts/main.js
+++ scripts/main.js
@@ -10,3 +10,40 @@ function closeNav() {
   document.getElementById("main").style.display = "block";
   document.body.style.backgroundColor = "#ffffff";
 }
+
+// Function to handle the typing effect
+function typeWriter(elementId, textArr) {
+  var count = 0;
+  var i = 0;
+  // speed for typing effect
+  var typingSpeed = 100;
+  // speed for interval between text strings
+  var intervalSpeed = 2000;
+
+  function type() {
+    if (count < textArr.length) {
+      if (i < textArr[count].length) {
+        document.getElementById(elementId).innerHTML += textArr[count][i];
+        i++;
+        setTimeout(type, typingSpeed);
+      } else {
+        setTimeout(function () {
+          i = 0;
+          count++;
+          document.getElementById(elementId).innerHTML = "";
+          setTimeout(type, typingSpeed);
+        }, intervalSpeed);
+      }
+    } else {
+      count = 0;
+      i = 0;
+      document.getElementById(elementId).innerHTML = "";
+      setTimeout(type, intervalSpeed);
+    }
+  }
+  setTimeout(type, intervalSpeed);
+}
+
+// Text Array
+var textArr1 = ["Product Designer", "Product Manager", "UX Designer"];
+typeWriter("textMe", textArr1);
